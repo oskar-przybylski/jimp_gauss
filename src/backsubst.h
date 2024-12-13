e196523c56dd0e927e0482c3b7b53b80c2801fe0
myne145 <antoni.kucz@gmail.com>
2024-12-13 09:20:23 +0100

    Dodano komentarze do zmiennych

--- src/backsubst.h
+++ src/backsubst.h
@@ -7,7 +7,10 @@
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
- */
+ * x wektor niewiadomych na ktorych pracuje
+ * mat macierz wspolczynnikow
+ * b wektor wyrazow wolnych
+*/
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b);
 
 #endif
